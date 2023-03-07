8c03dd4e93ddc18293b893dde17e15ae0175476c
shibaftw2022 <119848597+shibaftw2022@users.noreply.github.com>
2023-03-07 15:12:21 +0800

    second commit

--- src/components/User.js
+++ src/components/User.js
@@ -2,6 +2,9 @@ import classes from './User.module.css';
 import { Component } from 'react';
 
 class User extends Component {
+  componentWillUnmount(){
+    console.log('User will un mount');
+  }
   render(){
     return <li className={classes.user}>{this.props.name}</li>;
   }
